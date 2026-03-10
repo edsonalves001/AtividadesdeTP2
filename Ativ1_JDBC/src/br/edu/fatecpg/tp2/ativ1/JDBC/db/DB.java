9fe034b14f433d7106bbf18dccb83b96e7fb491e
Edson <105327286+edsonalves001@users.noreply.github.com>
2026-03-09 21:19:29 -0300

    Update DB.java

--- Ativ1_JDBC/src/br/edu/fatecpg/tp2/ativ1/JDBC/db/DB.java
+++ Ativ1_JDBC/src/br/edu/fatecpg/tp2/ativ1/JDBC/db/DB.java
@@ -7,12 +7,13 @@ public class DB {
     public static Connection connection() throws SQLDataException {
         try{
             var jdbcUrl = "jdbc:postgresql://localhost:5432/AulasdeTP2";
-            var user = "postgres";
-            var password = "78203861";
+            var user = "fatec";
+            var password = "fatec";
             return DriverManager.getConnection(jdbcUrl,user,password);
         }catch (Exception e){
             System.err.println(e.getMessage());
             return null;
         }
     }
+
 }
